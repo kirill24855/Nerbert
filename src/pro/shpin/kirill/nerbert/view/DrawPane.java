e95401110946924619df427a91214b578343567c
kirill.shpin <kirill.shpin@gmail.com>
2017-06-17 21:13:06 -0700

    Added better visuals for Pen tool

--- src/pro/shpin/kirill/nerbert/view/DrawPane.java
+++ src/pro/shpin/kirill/nerbert/view/DrawPane.java
@@ -36,16 +36,22 @@ public class DrawPane extends JPanel {
 		g2.setColor(new Color(color[0]));
 		for (PenObject object : penObjects) {
 			if (object.points.size() == 0) continue;
-			PenPoint curPoint = object.points.get(0);
 
-			g2.fillOval(curPoint.x, curPoint.y, 5, 5);
+			PenPoint curPoint = object.points.get(0);
 
 			if (object.points.size() > 1) {
 				Path2D path = new Path2D.Float();
 				path.moveTo(curPoint.x, curPoint.y);
-				for (int i = 1; i < object.points.size(); i++) {
+
+				for (int i = 0; i < object.points.size(); i++) {
 					PenPoint nextPoint = object.points.get(i);
-					g2.fillOval(nextPoint.x, nextPoint.y, 5, 5);
+					g2.fillOval(nextPoint.x-4, nextPoint.y-4, 8, 8);
+					if (object.selected == i) {
+						g2.drawLine(nextPoint.x, nextPoint.y, nextPoint.controlLastX, nextPoint.controlLastY);
+						g2.drawLine(nextPoint.x, nextPoint.y, nextPoint.controlNextX, nextPoint.controlNextY);
+						g2.drawOval(nextPoint.controlLastX-4, nextPoint.controlLastY-4, 8, 8);
+						g2.drawOval(nextPoint.controlNextX-4, nextPoint.controlNextY-4, 8, 8);
+					}
 
 					path.curveTo(
 							curPoint.controlNextX,
